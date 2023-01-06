7b2708ac2c04c1401ed53261e10878ff3bcf1d6b
Hudson Levine <gamem45000@gmail.com>
2023-01-06 16:30:03 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,15 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+    cout << "Press Enter 3 times to reveal your future.";
+    string gate[]={"A","B+", "B","C+","C", "D+", "D", "F","W"};
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    cout << "You will get " << gate[x] << " in this 261102."; 
+}
